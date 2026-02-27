dd084bbd57b2f620dc98cb7560c5a973611c68bb
hirokr <hirokr0625@gmail.com>
2026-02-27 19:27:10 +0600

    feat: add configuration settings and project structure documentation

---
+++ ai-server/app/core/config.py
@@ -0,0 +1,23 @@
+from os import name
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+from typing import Optional
+
+class Settings(BaseSettings):
+
+    MASTER_APIKEY: str = Field(..., validation_alias="MASTER_APIKEY")
+    SERPER_APIKEY: str = Field(..., validation_alias="SERPER_APIKEY")
+    OPEN_ROUTER_APIKEY: str = Field(..., validation_alias="OPEN_ROUTER_APIKEY")
+    
+    PROJECT_NAME: str = "TRYORA AI Server"
+    DEBUG: bool = False
+    DEBUG: bool = False
+
+    # Tell Pydantic to read from a .env file
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+settings = Settings() #type: ignore
+
+if name == "__main__":
+  print(settings.MASTER_APIKEY)
