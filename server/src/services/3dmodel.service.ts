028f2e018dcb7e20aa21e8e6ede4709f67736f3d
hirokr <hirokr0625@gmail.com>
2026-04-14 13:03:44 +0600

    feat: add initial 3D model service with TODOs for generation and management

---
+++ server/src/services/3dmodel.service.ts
@@ -0,0 +1,5 @@
+// todo: call the 3d generation engine
+// todo: store the generated 3d model in the database and associate it with the user
+// todo: create an endpoint to retrieve the generated 3d model for the user
+// todo: create an endpoint to delete the generated 3d model for the user
+// todo: create recursive polling to check the status of the 3d generation task and update the database accordingly
