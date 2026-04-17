dc0224496062c314f9d58ce3c86ba67ea78c4193
hirokr <hirokr0625@gmail.com>
2026-04-17 17:39:55 +0600

    feat: implement URL upload functionality using UTApi

--- server/src/utils/uploadthings.ts
+++ server/src/utils/uploadthings.ts
@@ -1,21 +1,24 @@
-import { createUploadthing, type FileRouter } from 'uploadthing/express';
+import 'dotenv/config';
+import { UTApi } from 'uploadthing/server';
 
-const f = createUploadthing();
+const utapi = new UTApi();
 
-export const uploadRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
-    image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
-      maxFileSize: '4MB',
-      maxFileCount: 1,
-    },
-  }).onUploadComplete(data => {
-    console.log('upload completed', data);
-  }),
-} satisfies FileRouter;
+type UploadResult = Awaited<ReturnType<typeof utapi.uploadFilesFromUrl>>;
 
-export type OurFileRouter = typeof uploadRouter;
+
+export async function handleUrlUpload(productUrl: string, fileName: string): Promise<UploadResult> {
+  try {
+    const response = await utapi.uploadFilesFromUrl({
+      url: productUrl,
+      name: fileName,
+    });
+
+    return [response];
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+
+// handleUrlUpload('https://avatars.githubusercontent.com/u/117710065?v=4', 'profile-image.png');
