7c1230f87868d413fbd97f81b90e4c9e4d3eb5f5
hirokr <hirokr0625@gmail.com>
2026-04-22 12:47:08 +0600

    refactor: specify CORS origin settings for improved security

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,11 @@ import searchRoutes from './routes/search.route.ts';
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
